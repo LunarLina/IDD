83e3c7bb666d864d35bd7ef91f504577a7e49b61
Lina <58235222+LunarLina@users.noreply.github.com>
2022-01-20 17:44:34 +0100

    Add files via upload

--- HipHipHop/hip.php
+++ HipHipHop/hip.php
@@ -1,27 +1,29 @@
 <html>
 <head>
-<h1>Hip Hip Hop</h1>
+<h1>Hip Hop</h1>
 </head>
 <body>
 
+
 <?php
+for($i = 1;$i<=100;$i++) {
+    echo '<br/>';
 
-for( $i=1; $i<=100; $i++ )
-{
-    //Om te checken of het klopt ↓
-    // echo $i //
-    if($i % 3 == 0) 
-{
-    echo "<br />";
-    echo "Hip";
-} 
-elseif ($i % 5 == 0) 
-{
-    echo "Hop";
-    echo "<br />";
-}
-}
-
+    if(($i%3 === 0)&&($i%5 === 0)) 
+    { echo 'HipHop';
+        continue; 
+    } else if($i % 3 === 0) 
+    { 
+        echo 'Hip';
+    } else if(($i %  5 === 0)) 
+    { 
+        echo 'Hop'; 
+    } else
+    { 
+        echo $i; 
+    }
+    
+    }
 ?>
 </body>
 
